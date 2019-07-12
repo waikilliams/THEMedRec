5f7fb74dc5d421aa01df997c87712f7800891b58
Williams <kaivw@s-ross-0230-m08.local>
2019-07-12 15:27:30 -0400

    adding messenger

---
+++ Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Bridging-Header.h
+//  THEMedRec
+//
+//  Created by Williams, Kai on 7/12/19.
+//  Copyright © 2019 Williams, Kai. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+
+#endif /* Bridging_Header_h */
---
+++ THEMedRec/ViewControllers/NewMessageController.swift
@@ -0,0 +1,13 @@
+//
+//  NewMessageController.swift
+//  THEMedRec
+//
+//  Created by Williams, Kai on 7/12/19.
+//  Copyright © 2019 Williams, Kai. All rights reserved.
+//
+
+import UIKit
+
+class NewMessageController: NSObject {
+
+}
